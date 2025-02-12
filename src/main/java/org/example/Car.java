afda73b94f53efe500884ee59e92bec384de3403
ruserxd <zxcmomo0219@gmail.com>
2025-02-13 07:15:12 +0800

    feat: add country member to Car class
    Add country information functionality:
    - Add country member to Car class
    - Create getter and setter for country property
    - Add showCountry method for display
    - Update constructor to include country parameter
    - Modify showInformation to display country info
    
    This addition helps track car manufacturing origin
    and enriches car information display.
    
    Ref #1

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -3,11 +3,13 @@ package org.example;
 public class Car {
     protected String carName;
     protected String productionDate;
+    protected String country;
 
     // car 的 construction
-    public Car(String name, String productionDate) {
+    public Car(String name, String productionDate, String country) {
         this.carName = name;
         this.productionDate = productionDate;
+        this.country = country;
     }
 
     // 獲取 car name
@@ -30,6 +32,16 @@ public class Car {
         this.productionDate = productionDate;
     }
 
+    // 獲取 car production date
+    public String getCountry() {
+        return this.country;
+    }
+
+    // 設定 car production date
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     // 輸出 car name
     void showCarName() {
         System.out.println("Car Name is " + getCarName());
@@ -40,11 +52,16 @@ public class Car {
         System.out.println("Car production date is " + this.productionDate);
     }
 
+    void showCountry() {
+        System.out.println("Car country is " + getCountry());
+    }
+
     // 輸出 car 所有資訊
     public void showInformation() {
         System.out.println("-------");
         showCarName();
         showCarProductionDate();
+        showCountry();
         System.out.println("-------");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Toyota", "2025/01/01");
+        Car car = new Car("Toyota", "2025/01/01", "Taiwan");
+
         // 測試 1
         car.showInformation();
+
         // 測試 2
         car.setCarName("Honda");
         car.showInformation();
+
     }
 }
